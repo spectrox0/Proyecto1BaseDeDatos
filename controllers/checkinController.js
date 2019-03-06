e341cf6bc8b358eaadbd2e51780cf1cd6a1e44de
spectrox0 <43557525+spectrox0@users.noreply.github.com>
2019-03-06 18:46:27 -0400

    checkFinal
    Esta función inserta en abordados y baja el número de asientos disponibles en la tabla "modeloasientos"

--- controllers/checkinController.js
+++ controllers/checkinController.js
@@ -24,9 +24,50 @@ exports.checkInOn = async (req, res) => {
 	let pasaporte = req.body.Pasaporte;
 	let nvuelo = req.body.NroVuelo;
 
-	// return res.redirect(`/checkInOn/${pasaporte}/${nvuelo}`);
-	return res.render('checkinOn',{pasaporte , nvuelo } );
+	// res.redirect(`/checkInOn/${pasaporte}/${nvuelo}`);
+	res.render('checkinOn', {pasaporte, nvuelo});
 };
-exports.checkInOn_Get = async (req, res) => {
-	return await res.render('checkinOn');
+
+exports.checkInOn_Get = (req, res) => {
+	console.log(req.body);
+	res.render('checkinOn');
 };
+
+exports.checkFinal = async (req, res) => {
+
+	sql.query('UPDATE abordados SET cant_Equipaje=:cant WHERE Pasaporte_P=:pasap AND C_vuelo=:vuelo',
+	{replacements:{
+		cant: req.body.nroEquipaje,
+		pasap:req.params.p,
+		vuelo:req.params.nv
+	}, type: sql.QueryTypes.UPDATE})
+		.catch(error => {
+			if(error) {
+				console.log(error);
+			}
+		});
+
+	let modelo = await sql.query('SELECT a.C_modelo FROM avion a, vuelo v WHERE v.C_vuelo=:vuelo AND v.C_avion=a.C_avion',
+	{replacements:{
+		vuelo:req.params.nv
+	}, type: sql.QueryTypes.SELECT})
+		.catch(error => {
+			if(error) {
+				console.log(error);
+			}
+		});
+
+	sql.query('UPDATE modeloasientos SET Cantidad=Cantidad-1 WHERE C_modelo=:modelo AND C_asiento=1 AND Cantidad > 0',
+	{replacements: {
+		modelo: modelo[0].C_modelo
+	}, type: sql.QueryTypes.UPDATE});
+	res.send("hola");
+};
+
+// update modeloasientos 
+// set Cantidad=Cantidad-1
+// where C_modelo=1 AND C_asiento=2 AND Cantidad > 19;
+
+// select a.C_modelo 
+// from avion a, vuelo v
+// where v.C_vuelo=1 and v.C_avion=a.C_avion;
